a26ac1398b991e3e3ceb06b088e399b9209b128e
cherylwu834 <124432604+cherylwu834@users.noreply.github.com>
2023-11-12 17:44:01 -0800

    Compare Resume Page: Fixed pdf link variable issue
    Was having problems when replacing the link to pdf with a variable that contained the link to pdf; fixed it by putting {} around the variable name

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 function Resume({ pdf_link, onButtonClick }) {
     return (
         <div className="resume-container">
-            <iframe src="http://africau.edu/images/default/sample.pdf" className="pdf" />
+            <iframe src={pdf_link} className="pdf" />
             <button className="resume-button" onClick={onButtonClick}> This One </button>
         </div>
     );
@@ -14,8 +14,8 @@ export default function Page() {
     const [pdf_1_link, setPdf_1_link] = useState(get_pdf_link()); //replace with function that returns the link
     const [pdf_2_link, setPdf_2_link] = useState(get_pdf_link()); //replace with function that returns the link
 
-    console.log('pdf_1_link: ' + pdf_1_link);
-    console.log('pdf_2_link: ' + pdf_2_link);
+    //console.log('pdf_1_link: ' + pdf_1_link);
+    //console.log('pdf_2_link: ' + pdf_2_link);
 
     function winClick(win,lose) { //Do we want to pass in link or ID? - let's do link, but convert to ID when calling the backend func
         console.log('Button clicked!'); // For example, log a message to the console
@@ -39,7 +39,6 @@ export default function Page() {
         
     )
 }
-
 
 
 function get_pdf_link() { //placeholder fucntion, might actually have to be one function that returns two links later
