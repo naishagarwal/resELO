90cc3ce00ed6c922d567c06f4fc4940965df084f
cherylwu834 <124432604+cherylwu834@users.noreply.github.com>
2023-11-30 18:13:51 -0800

    Sign In form done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Compare from './Pages/Compare/compare_page';
 import Upload from './Pages/Upload/upload_page';
 import Club_Page from './Pages/Club_Page/club_page';
 import User_Dashboard from './Pages/User_Dashboard/dashboard_Page'
+import Sign_Up from './Pages/Sign_Up_Page/sign_up_page';
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
          <Route path="/" element={<Upload />} />
          <Route path="/club-page" element={<Club_Page />} />
          <Route path="/dashboard" element={<User_Dashboard />} />
+         <Route path="/sign-up" element={<Sign_Up />} />
       </Routes>
       </BrowserRouter>
  );
---
+++ src/Pages/Sign_Up_Page/sign_up_page.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { useState } from 'react';
+import sign_up_styles from './sign_up_style.module.css';
+
+
+function sign_up () {
+    document.body.style.backgroundColor = '#3f4f37cc'; /*sets background color to green*/
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleSubmit() {
+        if (false){ /*TODO: Check if Username is already taken*/
+            alert('Username is already taken! Please select a different username.');
+            return;
+        }
+        if (username === "" || password === ""){
+            alert('Username and Password cannot be empty!');
+            return;
+        }
+        /*TODO: Add user to database, redirect them to the dashboard*/
+        alert('New Account Created! with username: ' + username + ' and password: '+  password);
+        setUsername("");
+        setPassword("");
+    }
+
+
+    return (
+        <div className={sign_up_styles.sign_up_container}>
+            <h1>Create An Account</h1>
+            <form 
+                className={sign_up_styles.form_container} 
+                onSubmit={(e) => {e.preventDefault(); handleSubmit(); }}
+            > 
+                <label>
+                    Username:
+                    <input 
+                        className={sign_up_styles.input_box} 
+                        type="text" 
+                        value = {username}
+                        placeholder="Username" 
+                        onChange={(e) => setUsername(e.target.value)}>
+                    </input>
+                </label>
+
+                <label>
+                    Password:
+                    <input 
+                        className={sign_up_styles.input_box} 
+                        type="password" 
+                        value = {password}
+                        placeholder="Password" 
+                        onChange={(e) => setPassword(e.target.value)}>
+                    </input>
+                </label>
+                    
+                <button className={sign_up_styles.submit_button} type="submit" >Submit</button>
+            </form>
+
+            <button className={sign_up_styles.sign_in_button} >Already have an account? Sign In</button>
+        </div>
+    );
+
+}
+
+export default sign_up;
