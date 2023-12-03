b8b0d73d812a897c14e0a6afcf65e1f0cfd37e65
Zane LaBute <zlabute@g.ucla.edu>
2023-12-03 14:29:57 -0800

    implemented search bar and funcitonality

--- src/Pages/Club_Page/club_page.js
+++ src/Pages/Club_Page/club_page.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState } from 'react';
+import SearchBar from './SearchBar';
 import club_styles from './club_page_style.module.css';
 
 function Counter({resumes_graded, total_resumes}) {
@@ -14,23 +15,21 @@ function Counter({resumes_graded, total_resumes}) {
   )
 }
 
-function Resumes({resumes}){ // Do we want resumes to be a link that can prese nt the pdf or just a name
- const resumeItems = resumes.map((resume,index) => ( // currently implemented as a list of strings
-    <div className={club_styles.resumeContainer} key={index}>{resume}</div>
-    )
-  );
 
- return (<div className={club_styles.scrollContainer}>
-  <div className={club_styles.scrollTitle}> Club Resumes </div>
-  {resumeItems}
- </div>)
-}
+
+
+
 
 export default function Page() {
-  const resume_list = get_resumes(); // replace with function to return list of resume objects
+  const resume_list = get_resumes();
+  const [search_results, set_search_results] = useState(resume_list);
   const resumes_total = resume_list.length;
   const resumes_graded = get_resumes_graded(); // replace with function to return number of graded resumes
 
+  const handleSearch = (searchTerm) => {
+    const filteredResults = resume_list.filter(item => item.toLowerCase().includes(searchTerm.toLowerCase()));
+    set_search_results(filteredResults);
+  };
   
 
   function back(){
@@ -43,13 +42,13 @@ export default function Page() {
 
   document.body.style.backgroundColor = "#3f4f37cc";
   
-  return(
+  return (
     <div>
       <button className={club_styles.backButton} onClick={back}> Back to Clubs page </button>
       <h1> Club Page </h1>
       <div className={club_styles.contentContainer}>
         <div className={club_styles.content}>
-          <Resumes resumes={resume_list} ></Resumes>
+          <Resumes resumes={search_results} handleSearch={handleSearch} ></Resumes>
         </div>
         <div className={club_styles.content}>
           <Counter resumesGraded={resumes_graded} resumeTotal={resumes_total}></Counter>
@@ -58,8 +57,24 @@ export default function Page() {
       </div>
     </div>
   )
-
 }
+
+function Resumes({resumes, handleSearch}){ // Do we want resumes to be a link that can prese nt the pdf or just a name
+  const resumeItems = resumes.map((resume,index) => ( // currently implemented as a list of strings
+     <div className={club_styles.resumeContainer} key={index}>{resume}</div>
+     )
+   );
+ 
+  return (<div className={club_styles.scrollContainer}>
+   <div className={club_styles.scrollTitleWithSearchBar}>
+             <div className={club_styles.scrollTitle}> Club Resumes </div>
+             <SearchBar handleSearch={handleSearch} />
+   </div>
+   {resumeItems}
+  </div>)
+ }
+
+
 
 function get_resumes() {
   //TODO: return a list of resume names or links
@@ -67,7 +82,8 @@ function get_resumes() {
   //filler
   let resumes = [];
   for (let i = 0; i < 100; i++){
-    resumes.push(i);
+    let b = String(i)
+    resumes.push(b);
   };
   return resumes;
 }
@@ -76,4 +92,5 @@ function get_resumes_graded() {
   // TODO: implement way to get number of resumes graded 
   return 3;
 }
+
 
