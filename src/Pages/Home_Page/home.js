bc55cf08a972655422f8ab829d078aad6ef2f202
ny-laa <nylaz@g.ucla.edu>
2023-12-03 20:30:47 -0800

    Home
    Added the image, linked the buttons, added contact info on bottom

--- src/Pages/Home_Page/home.js
+++ src/Pages/Home_Page/home.js
@@ -1,8 +1,8 @@
 import home_styles from './home.module.css';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
 
 const Home = () => {
-    document.body.style.backgroundColor = "#535945";
+    document.body.style.backgroundColor = "#C1A88A";
 
     return (
         <div>
@@ -10,19 +10,27 @@ const Home = () => {
                 WELCOME TO ResELO!
             </h1>
             <h2 className={home_styles.info_text}>
-                The premier resume ranking app! Simply have your applicants upload their resumes, and let the easy ranking begin!
+                The premier resume ranking app!<br/>
+                Simply have your applicants upload their resumes, and let the ranking begin!
             </h2>
+            <img src={require('./res_logo.PNG')} alt = "ResELO Logo" className={home_styles.home_logo}/>
             <div className={home_styles.buttons}>
-            <Link to="./Pages/SignUp/signup_page"><button className={home_styles.account_button}>
-                    SIGN IN
-                </button>
-                </Link>
+                <Link to='/login'>
+                    <button className={home_styles.account_button}>
+                        SIGN IN
+                    </button>
+                    </Link>
             </div>
             <div className={home_styles.buttons}>
+                <Link to='/sign-up'>
                 <button className={home_styles.account_button}>
                     CREATE ACCOUNT
                 </button>
+                </Link>
             </div>
+            <h2 className={home_styles.info_text}>
+                For questions, comments, or concerns, reach out to reselo@gmail.com!
+            </h2>
         </div>
 
     );
--- src/Pages/User_Dashboard/dashboard_Page.js
+++ src/Pages/User_Dashboard/dashboard_Page.js
@@ -2,27 +2,50 @@ import dash_styles from './dashboard.module.css';
 import { useState } from 'react'; 
 import { AddClub } from './add_club.js'; 
 import {Link} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
+import firebase from "firebase/auth";
+import { getAuth, } from "firebase/auth";
 
 //TODO: One issue is the buttons disappear when I refresh the page
 //this is because it is not being stored in databse rn; need to save clubs in database 
 
 function User_Dashboard() { 
+  const navigate = useNavigate();
   const [inputValue, setInputValue] = useState('');
   const [components, setComponents] = useState([]);
   document.body.style.backgroundColor = "#FCF9F5";
 
   const addComponent = (e) => {
     setInputValue(e.target.value);
+    
   };
 
   const addComponentChange = () => {
+    /* TODO: CHECK FOR DUPLICATE NAME*/
+    if (inputValue == ""){
+      return;
+    }
     setComponents([...components, { service: inputValue }]);
     setInputValue('');
   };
 
+  function logout() { //untested
+    const auth = getAuth();
+    auth.signOut()
+      .then(() => {
+        // Redirect to home page
+        navigate("/");
+      })
+      .catch((error) => {
+        alert('Error signing out:', error);
+      });
+    }
+
   return (
     <form>
       <div className={dash_styles.buttonDisplay}>
+      <button className={dash_styles.LogOutButton}
+                  onClick={logout}>Log Out</button>
       <h1 className={dash_styles.title}>USER'S DASHBOARD</h1>
           <div className={dash_styles.input}>
             <div>
